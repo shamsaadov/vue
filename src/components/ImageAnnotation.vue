bf361c55115c47a57174c69a33032be5e4c815c2
Said <said@MacBook-Air-Said.local>
2023-05-15 10:04:07 +0300

    добавил изменения концов отрезка

--- src/components/ImageAnnotation.vue
+++ src/components/ImageAnnotation.vue
@@ -8,6 +8,26 @@
                       :key="index"
                       :config="annotation.arrowConfig"
               ></v-line>
+              <v-circle
+                      v-for="(annotation, index) in annotations"
+                      :key="`start-${index}`"
+                      :config="annotation.startCircleConfig"
+                      @mouseover="changeCircleColor(annotation.startCircleConfig, 'green')"
+                      @mouseout="changeCircleColor(annotation.startCircleConfig, 'red')"
+                      @mousedown="startMovingStart(annotation)"
+                      @mouseup="stopMovingStart"
+              ></v-circle>
+
+              <v-circle
+                      v-for="(annotation, index) in annotations"
+                      :key="`end-${index}`"
+                      :config="annotation.endCircleConfig"
+                      @mouseover="changeCircleColor(annotation.endCircleConfig, 'green')"
+                      @mouseout="changeCircleColor(annotation.endCircleConfig, 'red')"
+                      @mousedown="startMovingEnd(annotation)"
+                      @mouseup="stopMovingEnd"
+              ></v-circle>
+
           </v-layer>
       </v-stage>
       <div class="annotation">
@@ -37,6 +57,8 @@ import img from "../assets/vue.png";
 export default {
   data() {
     return {
+      movingEnd: null,
+        movingStart: null,
       stageConfig: {
         width: window.innerWidth,
         height: window.innerHeight,
@@ -60,9 +82,9 @@ export default {
     this.$refs.stage.$el.addEventListener("mouseup", this.stopDrawing);
   },
   methods: {
-    loadImage() {
+      loadImage() {
       const imageObj = new Image();
-      imageObj.src = img;
+      imageObj.src = "http://catalog-mtz.ru/api/storage/media/images/q4pigMWyPAxixBcyYgBNnxhZfGp8X41FxFNZBZ8C.jpeg?expires=1683898683&signature=9fbef924b0c8f9284ed8a31e1507f64889083d8152a578ba1a388342fe779bb0";
       imageObj.onload = () => {
         this.imageConfig.image = imageObj;
         this.$nextTick(() => {
@@ -70,12 +92,32 @@ export default {
         });
       };
     },
+
+      startMovingEnd(annotation) {
+          this.movingEnd = annotation;
+      },
+
+
+      stopMovingEnd() {
+          this.movingEnd = null;
+      },
+
       startDrawing(event) {
           if (this.editingAnnotation) return;
+          if (this.movingEnd) return;
           this.drawing = true;
           const stage = this.$refs.stage.getStage();
           const pos = stage.getPointerPosition();
           this.startPoint = { x: pos.x, y: pos.y };
+
+          const circleConfig = {
+              x: this.startPoint.x,
+              y: this.startPoint.y,
+              radius: 10,
+              fill: 'red',
+              stroke: 'black',
+              strokeWidth: 1,
+          };
 
           const arrowConfig = {
               points: [
@@ -105,25 +147,55 @@ export default {
               id: Date.now(),
               arrowConfig,
               textConfig,
+              startCircleConfig: circleConfig,
+              endCircleConfig: { ...circleConfig },
           });
       },
+
+      changeCircleColor(circleConfig, color) {
+          circleConfig.fill = color;
+      },
+
       deleteAnnotation(index) {
           this.annotations.splice(index, 1);
       },
-      draw(event) {
-      if (!this.startPoint || this.editing) return;
-      const stage = this.$refs.stage.getStage();
-      const pos = stage.getPointerPosition();
-      const currentAnnotation = this.annotations[this.annotations.length - 1];
 
-      currentAnnotation.arrowConfig.points = [
-        this.startPoint.x,
-        this.startPoint.y,
-        pos.x,
-        pos.y,
-      ];
-    },
-    showInput(annotation, event) {
+      draw(event) {
+          const stage = this.$refs.stage.getStage();
+          const pos = stage.getPointerPosition();
+
+          if (this.movingEnd) {
+              this.movingEnd.endCircleConfig.x = pos.x;
+              this.movingEnd.endCircleConfig.y = pos.y;
+              this.movingEnd.arrowConfig.points[2] = pos.x;
+              this.movingEnd.arrowConfig.points[3] = pos.y;
+          } else if (this.movingStart) {
+              this.movingStart.startCircleConfig.x = pos.x;
+              this.movingStart.startCircleConfig.y = pos.y;
+              this.movingStart.arrowConfig.points[0] = pos.x;
+              this.movingStart.arrowConfig.points[1] = pos.y;
+          } else if (this.startPoint) {
+              const currentAnnotation = this.annotations[this.annotations.length - 1];
+              currentAnnotation.arrowConfig.points = [
+                  this.startPoint.x,
+                  this.startPoint.y,
+                  pos.x,
+                  pos.y,
+              ];
+              currentAnnotation.endCircleConfig.x = pos.x;
+              currentAnnotation.endCircleConfig.y = pos.y;
+          }
+      },
+
+      startMovingStart(annotation) {
+          this.movingStart = annotation;
+      },
+
+      stopMovingStart() {
+          this.movingStart = null;
+      },
+
+      showInput(annotation, event) {
       if (!this.drawing) {
         this.editing = true;
         this.editingText = annotation.textConfig.text;
@@ -135,44 +207,39 @@ export default {
         inputElement.focus();
       }
     },
+
       removeAnnotation(annotation) {
           const index = this.annotations.findIndex(a => a.id === annotation.id);
           if (index !== -1) {
               this.annotations.splice(index, 1);
           }
       },
-    stopDrawing() {
-      if (this.editing) return;
-      const currentAnnotation = this.annotations[this.annotations.length - 1];
-      const points = currentAnnotation.arrowConfig.points;
-      const distance = Math.sqrt(
-        Math.pow(points[2] - points[0], 2) + Math.pow(points[3] - points[1], 2)
-      );
 
-      if (distance < 20) {
-        this.annotations.pop();
-      }
+      stopDrawing() {
+          this.startPoint = null;
+          this.movingEnd = null;
+          this.movingStart = null;
+      },
 
-      this.startPoint = null;
-      this.drawing = false;
-    },
+
       toggleEdit(annotation) {
           annotation.editing = !annotation.editing;
           if (!annotation.editing) {
               this.endEditText(annotation);
           }
       },
+
       startEditText(annotation, event) {
           annotation.editing = true;
           this.editingAnnotation = annotation;
       },
+
       endEditText(annotation) {
           annotation.editing = false;
           this.editingAnnotation = null;
       },
 
-
-    logAnnotations() {
+      logAnnotations() {
       console.log("Annotations:");
       this.annotations.forEach((annotation, index) => {
         console.log(`Annotation ${index + 1}:`);
@@ -181,7 +248,8 @@ export default {
         console.log(`Text: ${annotation.textConfig.text}`);
       });
     },
-    getTextConfig(annotation, index) {
+
+      getTextConfig(annotation, index) {
       return {
         ...annotation.textConfig,
         x: 10,
